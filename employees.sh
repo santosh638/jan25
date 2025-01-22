cf7338eba2dd222c9dbc92f692258ac98baa5e00
santosh <santoshathani42@gmail.com>
2025-01-22 21:46:35 +0530

    work files added

---
+++ employees.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# File containing employee data
+# EMPLOYEE_FILE="employees.txt"
+#
+# # Check if the file exists
+# if [[ ! -f $EMPLOYEE_FILE ]]; then
+#   echo "Error: $EMPLOYEE_FILE not found!"
+#     exit 1
+#     fi
+#
+#     # Display header
+#     echo "Employees with age greater than 40:"
+#
+#     # Read and filter employees
+#     awk -F'\t' 'NR > 1 && $3 > 40 { print $1 }' "$EMPLOYEE_FILE"
